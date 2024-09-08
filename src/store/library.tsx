a4500037eaadb1c2c0ef87a10d5d3b832d53273f
martinromario55 <martinromario55@gmail.com>
2024-09-08 03:53:54 +0300

    Add To playlist

--- src/store/library.tsx
+++ src/store/library.tsx
@@ -91,3 +91,29 @@ export const usePlayLists = () => {
 
 	return { playlists, addToPlaylist }
 }
+
+export const usePlaylists = () => {
+	const playlists = useLibraryStore((state) => {
+		return state.tracks.reduce((acc, track) => {
+			track.playlist?.forEach((playlistName) => {
+				const existingPlaylist = acc.find((playlist) => playlist.name === playlistName)
+
+				if (existingPlaylist) {
+					existingPlaylist.tracks.push(track)
+				} else {
+					acc.push({
+						name: playlistName,
+						tracks: [track],
+						artworkPreview: track.artwork ?? unknownTrackImageUri,
+					})
+				}
+			})
+
+			return acc
+		}, [] as Playlist[])
+	})
+
+	const addToPlaylist = useLibraryStore((state) => state.addToPlaylist)
+
+	return { playlists, addToPlaylist }
+}
