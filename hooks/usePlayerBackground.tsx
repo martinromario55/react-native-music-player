28aad143f54ce4b8e23993457f4b675e56bcca58
martinromario55 <martinromario55@gmail.com>
2024-09-09 09:58:52 +0300

    Android Image Color

--- hooks/usePlayerBackground.tsx
+++ hooks/usePlayerBackground.tsx
@@ -1,17 +1,26 @@
 import { colors } from '@/constants/tokens'
 import { useEffect, useState } from 'react'
+import { Platform } from 'react-native'
 import { getColors } from 'react-native-image-colors'
-import { IOSImageColors } from 'react-native-image-colors/build/types'
+import { IOSImageColors, AndroidImageColors } from 'react-native-image-colors/build/types'
 
 export const usePlayerBackground = (imageUrl: string) => {
-	const [imageColors, setImageColors] = useState<IOSImageColors | null>(null)
+	const [imageColors, setImageColors] = useState<IOSImageColors | AndroidImageColors | null>(null)
 
 	useEffect(() => {
 		getColors(imageUrl, {
 			fallback: colors.background,
 			cache: true,
 			key: imageUrl,
-		}).then((colors) => setImageColors(colors as IOSImageColors))
+		}).then((colors) => {
+			if (Platform.OS === 'ios') {
+				setImageColors(colors as IOSImageColors)
+			} else if (Platform.OS === 'android') {
+				setImageColors(colors as AndroidImageColors)
+			} else {
+				setImageColors(null)
+			}
+		})
 	}, [imageUrl])
 
 	return { imageColors }
